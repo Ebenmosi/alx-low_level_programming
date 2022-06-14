f99ba1b5735667314b3de9eccfb5b2a54f2d19b9
blessing akinola <ebenmosiakin@gmail.com>
2022-06-14 16:20:49 -0700

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/** 
+/**
+* swap_int - main
+* @a: pointer
+* @b: pointer 
+*/ 
+
+       void swap_int(int *a, int *b) 
+
+	void swap_int(int *a, int *b)
+
+{ 
+
+	int swap;
+}	
