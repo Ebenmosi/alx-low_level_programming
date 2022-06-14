d2be587d7b186908954440adb5275782fa79214f
blessing akinola <ebenmosiakin@gmail.com>
2022-06-14 16:50:42 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * @_strlen - function that Returns the length of a string
+ * @s: pointer to an string
+ * Return: int
+ */
+
+int _strlen(char *s)
+
+{
+
+	int i = 0;
+
+	while (s[i] != '\0')
+
+	{
+
+		i += 1;
+
+											}
+
+	return (i);
+}
