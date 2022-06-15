6c4c1fc7317070b2a874e1f76bf113a530af5178
blessing akinola <ebenmosiakin@gmail.com>
2022-06-14 17:56:14 -0700

    task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,25 +10,15 @@
 
 void print_rev(char *s)
 {
-	int i = 0;
+int i = 0;
 
-
-	while (*(s + i) != '\0')
-
-	i += 1;
-
-	i -= 1;
-
-	while (i >= 0)
-
-	{
-
-		_putchar(*(s + i));
-
-		i--;
-
-	}
-
-	_putchar('\n');
-
+while (*(s + i) != '\0')
+i += 1;
+i -= 1;
+while (i >= 0)
+{
+_putchar(*(s + i));
+i--;
+}
+_putchar('\n');
 }
